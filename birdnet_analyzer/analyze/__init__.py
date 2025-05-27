8268b90e29af4c6bfb4265b6867156ea7c1bcede
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-05-27 16:57:15 +0200

    added test case

--- birdnet_analyzer/analyze/__init__.py
+++ birdnet_analyzer/analyze/__init__.py
@@ -10,7 +10,7 @@ POSSIBLE_ADDITIONAL_COLUMNS_MAP = {
     "overlap": lambda: cfg.SIG_OVERLAP,
     "sensitivity": lambda: cfg.SIGMOID_SENSITIVITY,
     "min_conf": lambda: cfg.MIN_CONFIDENCE,
-    "species_list": lambda: cfg.SPECIES_LIST_FILE,
+    "species_list": lambda: cfg.SPECIES_LIST_FILE or "",
     "model": lambda: os.path.basename(cfg.MODEL_PATH),
 }
 
