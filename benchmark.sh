2b4598ce422042d68818466716c6863835103de3
Max Mauermann <max-mauermann@web.de>
2025-08-25 11:56:34 +0200

    bench cores

--- benchmark.sh
+++ benchmark.sh
@@ -37,36 +37,37 @@ run_benchmark() {
 
 LARGE_FILES="/data/testing_audio/medium_sized_soundscapes/" # 155
 SMALL_FILES="/data/testing_audio/small_files/" # 14018
+TEST_CORES="10"
 
 # warmup
-run_benchmark "V1" "0" "$LARGE_FILES" "10" "16"
+run_benchmark "V1" "0" "$LARGE_FILES" "$TEST_CORES" "16"
 
 # larger files
-run_benchmark "V1" "0" "$LARGE_FILES" "10" "16"
-run_benchmark "V2" "1" "$LARGE_FILES" "10" "16"
-run_benchmark "V2" "2" "$LARGE_FILES" "10" "16"
-run_benchmark "V2" "3" "$LARGE_FILES" "10" "16"
-run_benchmark "V2" "7" "$LARGE_FILES" "10" "16" # (155 files // 10 cores) // 2
-run_benchmark "V2" "10" "$LARGE_FILES" "10" "16"
-run_benchmark "V2" "15" "$LARGE_FILES" "10" "16" # 155 files // 10 cores
-run_benchmark "V3" "1" "$LARGE_FILES" "10" "16"
-run_benchmark "V3" "2" "$LARGE_FILES" "10" "16"
-run_benchmark "V3" "3" "$LARGE_FILES" "10" "16"
-run_benchmark "V3" "10" "$LARGE_FILES" "10" "16"
-run_benchmark "V3" "10" "$LARGE_FILES" "10" "16" # (155 files // 10 cores) // 2
-run_benchmark "V3" "15" "$LARGE_FILES" "10" "16" # 155 files // 10 cores
+run_benchmark "V1" "0" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "1" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "2" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "3" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "7" "$LARGE_FILES" "$TEST_CORES" "16" # (155 files // 10 cores) // 2
+run_benchmark "V2" "10" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "15" "$LARGE_FILES" "$TEST_CORES" "16" # 155 files // 10 cores
+run_benchmark "V3" "1" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "2" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "3" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "10" "$LARGE_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "10" "$LARGE_FILES" "$TEST_CORES" "16" # (155 files // 10 cores) // 2
+run_benchmark "V3" "15" "$LARGE_FILES" "$TEST_CORES" "16" # 155 files // 10 cores
 
 # small files
-run_benchmark "V1" "0" "$SMALL_FILES" "10" "16"
-run_benchmark "V2" "1" "$SMALL_FILES" "10" "16"
-run_benchmark "V2" "2" "$SMALL_FILES" "10" "16"
-run_benchmark "V2" "3" "$SMALL_FILES" "10" "16"
-run_benchmark "V2" "7" "$SMALL_FILES" "10" "16" # (14018 files // 10 cores) // 2
-run_benchmark "V2" "700" "$SMALL_FILES" "10" "16"
-run_benchmark "V2" "1401" "$SMALL_FILES" "10" "16" # 14018 files // 10 cores
-run_benchmark "V3" "1" "$SMALL_FILES" "10" "16"
-run_benchmark "V3" "2" "$SMALL_FILES" "10" "16"
-run_benchmark "V3" "3" "$SMALL_FILES" "10" "16"
-run_benchmark "V3" "10" "$SMALL_FILES" "10" "16"
-run_benchmark "V3" "700" "$SMALL_FILES" "10" "16" # (14018 files // 10 cores) // 2
-run_benchmark "V3" "1401" "$SMALL_FILES" "10" "16" # 14018 files // 10 cores
+run_benchmark "V1" "0" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "1" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "2" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "3" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "7" "$SMALL_FILES" "$TEST_CORES" "16" # (14018 files // 10 cores) // 2
+run_benchmark "V2" "700" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V2" "1401" "$SMALL_FILES" "$TEST_CORES" "16" # 14018 files // 10 cores
+run_benchmark "V3" "1" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "2" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "3" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "10" "$SMALL_FILES" "$TEST_CORES" "16"
+run_benchmark "V3" "700" "$SMALL_FILES" "$TEST_CORES" "16" # (14018 files // 10 cores) // 2
+run_benchmark "V3" "1401" "$SMALL_FILES" "$TEST_CORES" "16" # 14018 files // 10 cores
