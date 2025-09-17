5595b824471f2f07404b86ab72990d4481fc4d56
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2025-09-17 12:39:29 +0200

    added tests for model download + updated workflows

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,12 @@
+import os
 from pathlib import Path
+
+import pytest
 
 import birdnet_analyzer.config as cfg
 from birdnet_analyzer import utils
+
+IS_GITHUB_RUNNER = os.environ.get("IS_GITHUB_RUNNER", "false") == "true"
 
 
 def test_read_lines_label_files():
@@ -26,3 +31,64 @@ def test_read_lines_label_files():
             names = line.split("_")
             assert len(names) == 2, f"Expected two names in {line}, but got {len(names)} in {label}"
             assert original_labels[i][0] == names[0], f"Expected {original_labels[i][0]} but got {names[0]} in {label}"
+
+
+@pytest.mark.skipif(not IS_GITHUB_RUNNER, reason="Skip tests locally to avoid downloading large files")
+@pytest.mark.order("first")
+def test_birdnet_download():
+    download_path = Path(cfg.BIRDNET_MODEL_PATH)
+
+    # Does not exist in the repo before
+    assert not download_path.parent.exists()
+
+    utils.ensure_model_exists()
+
+    assert download_path.exists()
+
+
+@pytest.mark.skipif(not IS_GITHUB_RUNNER, reason="Skip tests locally to avoid downloading large files")
+@pytest.mark.order("second")
+def test_perch_download():
+    download_path = Path(cfg.PERCH_V2_MODEL_PATH)
+
+    # Does not exist in the repo before
+    assert not download_path.exists()
+
+    utils.ensure_model_exists(check_perch=True)
+
+    assert download_path.exists()
+    assert (download_path / "saved_model.pb").exists()
+    assert (download_path / "assets" / "labels.csv").exists()
+    assert (download_path / "variables" / "variables.data-00000-of-00001").exists()
+    assert (download_path / "variables" / "variables.index").exists()
+
+
+@pytest.mark.skipif(not IS_GITHUB_RUNNER, reason="Skip tests locally, because files likely exist for development.")
+@pytest.mark.order(after="test_birdnet_download")
+def test_download_if_birdnet_exists():
+    download_path = Path(cfg.BIRDNET_MODEL_PATH)
+    assert download_path.exists()
+
+    utils.ensure_model_exists()
+
+    assert download_path.exists()
+
+
+@pytest.mark.skipif(not IS_GITHUB_RUNNER, reason="Skip tests locally, because files likely exist for development.")
+@pytest.mark.order(after="test_perch_download")
+def test_download_if_perch_exists():
+    download_path = Path(cfg.PERCH_V2_MODEL_PATH)
+
+    assert download_path.exists()
+    assert (download_path / "saved_model.pb").exists()
+    assert (download_path / "assets" / "labels.csv").exists()
+    assert (download_path / "variables" / "variables.data-00000-of-00001").exists()
+    assert (download_path / "variables" / "variables.index").exists()
+
+    utils.ensure_model_exists(check_perch=True)
+
+    assert download_path.exists()
+    assert (download_path / "saved_model.pb").exists()
+    assert (download_path / "assets" / "labels.csv").exists()
+    assert (download_path / "variables" / "variables.data-00000-of-00001").exists()
+    assert (download_path / "variables" / "variables.index").exists()
