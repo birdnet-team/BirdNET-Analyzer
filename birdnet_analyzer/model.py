0f5ffbdf4ff4f97e6059b082ca5aa9827003e60f
Max Mauermann <max-mauermann@web.de>
2025-09-23 16:13:04 +0200

    early stopping monitors validation loss again instead of validation AUPRC

--- birdnet_analyzer/model.py
+++ birdnet_analyzer/model.py
@@ -754,8 +754,8 @@ def train_linear_classifier(
     callbacks = [
         # EarlyStopping with restore_best_weights
         keras.callbacks.EarlyStopping(
-            monitor="val_AUPRC",
-            mode="max",
+            monitor="val_loss",
+            mode="min",
             patience=patience,
             verbose=1,
             min_delta=min_delta,
