76c7a604a5f1440237f1b0e67d4180d53501773a
Josef Haupt <josef.haupt@informatik.tu-chemnitz.de>
2026-05-14 12:07:45 -0400

    set nr of workers to 1 for search to speedup process

--- birdnet_analyzer/search/utils.py
+++ birdnet_analyzer/search/utils.py
@@ -86,7 +86,7 @@ def get_query_embedding(
     else:
         sig_splits = audio.split_signal(sig, rate, sig_length, crop_overlap, sig_minlen)
 
-    return model_utils.get_embeddings_array(sig_splits)
+    return model_utils.get_embeddings_array(sig_splits, n_workers=1)
 
 
 def get_search_results(
